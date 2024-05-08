28bf316f7cae6ddbb3a237a3f1a25088b3e55586
Stacie Tilsley <stacietilsley98@gmail.com>
2024-05-08 16:04:33 +0100

    created homepage basics

--- PlotTwist/src/routes/homePage/homePage.jsx
+++ PlotTwist/src/routes/homePage/homePage.jsx
@@ -1,12 +1,29 @@
 import { Link } from "react-router-dom";
-
+import HomePageBG from "../../assets/HomePageBG.png";
+import FullLogo from "../../assets/FullLogo.png";
+import "./homePage.css";
 export default function HomePage() {
-    return (
-      <>
-      <h1>Home Page</h1>
-          <img />
-          <button />
-          <button />
-      </>
-    );
-  }
+  return (
+    <>
+      <div
+        className="container-full-length"
+        style={{
+          backgroundImage: `url(${HomePageBG})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="container-full-length__container">
+          <div className=" logo">
+            <img id="logo-img" src={FullLogo}></img>
+          </div>
+          <div className="buttons">
+            <button>My Saved Routes</button>
+            <button>Create A Route</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
