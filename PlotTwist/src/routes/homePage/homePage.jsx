1d471932a0184ceb2a92b8ca65925f006d870c85
Stacie Tilsley <stacietilsley98@gmail.com>
2024-05-08 15:17:59 +0100

    added assets folder

--- PlotTwist/src/routes/homePage/homePage.jsx
+++ PlotTwist/src/routes/homePage/homePage.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
-    return (
-      <>
+  return (
+    <>
       <h1>Home Page</h1>
-          <img />
-          <button />
-          <button />
-      </>
-    );
-  }
+      <img />
+      <button />
+      <button />
+    </>
+  );
+}
