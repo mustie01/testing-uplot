c9bcc0117542504380c2fc2c6e8c239badbfe207
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-05-09 11:40:08 +0100

    basic table configured

--- PlotTwist/src/routes/savedRoutesPage/savedRoutes.jsx
+++ PlotTwist/src/routes/savedRoutesPage/savedRoutes.jsx
@@ -1,9 +1,46 @@
 import Header from "../../components/headerComponent";
 
 export default function SavedRoutesPage() {
+  // dummy data
+  const data = [
+    {
+      name: "Test Route 1",
+      data: "Test Data 1",
+    },
+    {
+      name: "Test Route 2",
+      data: "Test Data 2",
+    },
+    {
+      name: "Test Route 3",
+      data: "Test Data 3",
+    },
+  ];
+
   return (
     <div>
       <Header />
+      {/* table with .map - show name and button */}
+      <table>
+        <tbody>
+          <tr>
+            <th>Route Name</th>
+            <th>Button</th>
+          </tr>
+          {data.map((route, index) => {
+            return (
+              <tr key={index}>
+                <td>{route.name}</td>
+                <td>
+                  <button>Retrieve Route</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
+
+// opening para graph
